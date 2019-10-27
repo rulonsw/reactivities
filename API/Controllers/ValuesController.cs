62916515578f6ae0c7676497f1008689ce68bb84
Rulon Wood <rulonsw@outlook.com>
2019-10-27 06:10:50 -0600

    First commit, post-migration script

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "value1", "value2"
+        };
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Summaries;
+        }
+
+        // GET api/WeatherForecast/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
